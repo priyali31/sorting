b2f5c9f40707d3257555e76a1b7dc6bd9671f7d3
priyali sakhare <priyalisakhare31@gmail.com>
2020-05-03 19:09:25 +0530

    added a.sh

--- a.sh
+++ a.sh
@@ -23,3 +23,27 @@ do
   array[i]=${assoarray[$ele]};
   i=$(($i+1));
 done
+
+for ((i = 0; i<4; i++)) 
+do
+      
+    for((j = 0; j<4-1; j++)) 
+    do
+      
+      k=$(($j+1));
+        if [ ${array[$j]} -gt ${array[$k]} ] 
+        then
+            # swap
+
+ 
+            temp=${array[j]} ;
+            array[$j]=${array[k]};   
+            array[$k]=$temp ;
+        fi
+    done
+  
+
+done
+
+
+echo "values in ascending order  ${array[@]}";
